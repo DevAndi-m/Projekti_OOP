f3aa950352a007c000ebdb75bf29359a5cf6dae1
edrejta <endr224al@gmail.com>
2025-11-17 23:23:11 +0100

    Te klasa Patient e kom bo ni blueprint qysh me vazhdu

---
+++ src/healthapp/model/Patient.java
@@ -0,0 +1,16 @@
+package healthapp.model;
+
+public class Patient extends Person {
+    private int age;
+
+    public Patient(int id, String name, String phone, String email, int age) {
+        super(id, name, phone, email);
+        this.age = age;
+    }
+
+    @Override
+    public String getDetails() {
+        return String.format("Pacienti: %s (ID: %d), Mosha: %d, Telefoni: %s, Email: %s",
+                getName(), getId(), age, getPhone(), getEmail());
+    }
+}
---
+++ src/healthapp/model/Person.java
@@ -0,0 +1,66 @@
+package healthapp.model;
+
+import java.util.Objects;
+
+public abstract class Person {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public abstract String getDetails();
+
+    public Person(int id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Person other){
+            return other.id == id;
+        }
+        return false;
+    }
+
+    @Override
+
+    public int hashCode(){
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Id: %d, Emri: %s, Telefoni: %s, Email: %s", id, name, phone, email);
+    }
+}
